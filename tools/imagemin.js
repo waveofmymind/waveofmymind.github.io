729d16719fc24468a804eddfe4c2b02eb48098f4
haril song <songkg7@gmail.com>
2023-04-18 14:06:03 +0900

    feat: image obtimize for SEO

---
+++ tools/imagemin.js
@@ -0,0 +1,38 @@
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import fs from 'fs';
+import path from 'path';
+
+async function deleteFilesInDirectory(dir) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach(function (file) {
+    const filePath = path.join(dir, file);
+    const extname = path.extname(filePath);
+    if (extname === '.png' || extname === '.jpg' || extname === '.jpeg') {
+      fs.unlinkSync(filePath);
+      console.log(`remove ${filePath}`);
+    }
+  });
+}
+
+async function convertImages(dir) {
+  const subDirs = fs
+    .readdirSync(dir)
+    .filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
+
+  await imagemin([`${dir}/*.{png,jpg,jpeg}`], {
+    destination: dir,
+    plugins: [imageminWebp({ quality: 75 })]
+  });
+  await deleteFilesInDirectory(dir);
+
+  for (const subDir of subDirs) {
+    const subDirPath = path.join(dir, subDir);
+    await convertImages(subDirPath);
+  }
+}
+
+(async () => {
+  await convertImages('assets/img');
+})();
